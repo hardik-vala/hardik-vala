15aff81d13e9f93a60493c1012f20c5208ad12ce
Hardik Vala <hardikvala24@gmail.com>
2025-10-12 19:53:24 +0000

    Update copy.

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="my-40 max-w-2xl mx-auto text-center">
       <h1 className="text-3xl font-medium mb-4">The technical founder</h1>
-      <h1 className="text-2xl text-gray-500">YC S23 | ex-Google | ex-Apple</h1>
+      <h1 className="text-2xl text-gray-500">yc s23 | ex-google | ex-apple</h1>
     </div>
   );
 }
