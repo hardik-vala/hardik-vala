cc42df461ac6c1362a85b117edeeb31cd4b50b82
Hardik Vala <hardikvala24@gmail.com>
2025-10-12 19:58:10 +0000

    Update formatting.

--- nextjs/src/app/blog/page.tsx
+++ nextjs/src/app/blog/page.tsx
@@ -9,10 +9,10 @@ export default function Posts() {
   return (
     <PageLayout title="Blog">
       <div>
-        <ul className="space-y-4">
+        <ul className="space-y-2">
           {posts.map((post) => (
             <li key={post.slug}>
-              <div className="flex flex-row gap-8">
+              <div className="flex flex-row gap-4">
                 <span className="w-[25%] md:w-[20%] lg:w-[15%] text-gray-400 text-sm text-right">
                   {new Date(post.date).toLocaleDateString("en-US", {
                     month: "short",
