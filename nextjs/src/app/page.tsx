b073c7f5ac43044926909a1e319f7b0b383b1dd5
Hardik Vala <hardikvala24@gmail.com>
2025-12-14 21:34:35 -0800

    update title

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/dist/client/link";
-
 export default function Home() {
   return (
     <div className="my-40 max-w-2xl mx-auto text-center">
-      <h1 className="text-3xl font-medium mb-6">The technical founder</h1>
-      <h1 className="text-2xl font-medium mb-6"><Link href="https://qaggle.com" className="font-medium text-blue-600 hover:text-blue-500 transition-colors">qaggle</Link> <span className="text-gray-400">& one over one</span></h1>
+      <h1 className="text-3xl font-medium mb-6">technical founder</h1>
       <h1 className="text-xl text-gray-600">yc s23 | ex-google | ex-apple</h1>
     </div>
   );
