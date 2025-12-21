b5e5baf1f8de4fcbe406ba49f2fa144900412a59
Hardik Vala <hardikvala24@gmail.com>
2025-12-20 20:19:13 -0800

    drop company

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -39,7 +39,6 @@ export default function RootLayout({
 }>) {
   const navigationItems = [
     { name: "Blog", link: "/blog" },
-    { name: "Company", link: "https://qaggle.com", target: "_blank" },
     { name: "Projects", link: "/projects" },
   ];
 
