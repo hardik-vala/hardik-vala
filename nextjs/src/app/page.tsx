730813c0b158111de96eb74f2b7040b3c8872819
Hardik Vala <hardikvala24@gmail.com>
2026-06-28 13:29:16 -0700

    update landing page title

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="my-40 max-w-2xl mx-auto text-center">
-      <h1 className="text-3xl font-medium mb-6">technical founder</h1>
+      <h1 className="text-3xl font-medium mb-6">i'm technical</h1>
       <h1 className="text-xl text-gray-600">yc s23 | ex-google | ex-apple</h1>
     </div>
   );
